38ee2f26fef56425dfc58178598b44a620fd8bca
forc17c <85665007+forc17c@users.noreply.github.com>
2021-08-14 20:06:59 +0900

    MONGODB AND MONGOOSE

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,16 +1,18 @@
 import express from "express";
 import {
-  see,
-  edit,
-  upload,
+  watch,
+  getUpload,
+  getEdit,
+  postEdit,
+  postUpload,
   deleteVideo,
-} from "../controllers/videoController.js";
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see);
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
